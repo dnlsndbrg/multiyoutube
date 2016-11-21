243dfa57ba395c23209ca7cb1f7bd4c594ee50f5
dnlsndbrg <dnlsndbrg@gmail.com>
2016-11-21 22:42:23 +0100

    :red_circle: test for onYouTubeIframeAPIReady

--- test/YoutubePlayer.test.js
+++ test/YoutubePlayer.test.js
@@ -1,14 +1,24 @@
 let should = require('chai').should();
+let sinon = require('sinon');
+
 let YoutubePlayer = require('../src/client/YoutubePlayer');
 
-describe('Youtube Class', () => {
+describe('Youtube Class', function() {
     let sut;
 
-    beforeEach(() => {
+    beforeEach(function() {
         sut = new YoutubePlayer();
     });
 
-    it('should be an object', () => {
+    it('should be an object', function() {
         sut.should.be.an('object');
+    });
+
+    describe('onYouTubeIframeAPIReady', function() {
+        it('should create a player', () => {
+            let iframeAPI = { Player: sinon.stub() };
+            sut.onYouTubeIframeAPIReady(iframeAPI);
+            sut.player.should.exist;
+        });
     });
 });
