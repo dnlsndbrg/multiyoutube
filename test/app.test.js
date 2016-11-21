8b5a1a1d9fe64d525957503e085e223d893a2bb6
dnlsndbrg <dnlsndbrg@gmail.com>
2016-11-21 22:51:15 +0100

    :repeat: adds test for 404

--- test/app.test.js
+++ test/app.test.js
@@ -3,16 +3,24 @@ let app = require('./../src/server/app');
 
 describe('Express web server', () => {
 
-    it('Returns status code 200', (done) => {
-        request(app)
+    describe('GET /', () => {
+        it('Returns status code 200', (done) => {
+            request(app)
             .get('/')
             .expect(200, done);
-    });
+        });
 
-    it('Should return HTML', (done) => {
-        request(app)
+        it('Should return HTML', (done) => {
+            request(app)
             .get('/')
             .expect('Content-Type', /html/, done);
+        });
+    });
+
+    it('Returns status code 404 on non existant route', (done) => {
+        request(app)
+            .get('/doesNotExist')
+            .expect(404, done);
     });
 
     it('Serves static content', (done) => {
