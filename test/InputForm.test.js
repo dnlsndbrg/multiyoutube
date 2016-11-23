7a242c268cb4483823a88e56604f8e8be856c6b8
dnlsndbrg <dnlsndbrg@gmail.com>
2016-11-23 21:09:06 +0100

    :red_circle:

--- test/InputForm.test.js
+++ test/InputForm.test.js
@@ -2,7 +2,7 @@
 
 let sinon = require('sinon');
 let should = require('chai').should();
-let InputField = require('../src/client/InputForm');
+let InputForm = require('../src/client/InputForm');
 
 
 describe('Input form for youtube urls', function() {
@@ -14,20 +14,29 @@ describe('Input form for youtube urls', function() {
     beforeEach(function() {
         let input = document.createElement('input');
         let button = document.createElement('button');
-        this.sut = new InputField(input, button);
+        let youtubePlayer = { changeVideo: sinon.spy() };
+        this.sut = new InputForm(input, button, youtubePlayer);
     });
 
     after(function() {
         this.jsdom();
     });
 
-    it('should prevent default submit event', function() {
-        let event = {
-            type: 'click',
-            preventDefault: sinon.spy()
-        };
-        this.sut.submit(event);
-        event.preventDefault.called.should.be.true;
-        event.preventDefault.reset();
+    describe('submit', function() {
+        it('should prevent default submit event', function() {
+            let event = {
+                type: 'click',
+                preventDefault: sinon.spy()
+            };
+            this.sut.submit(event);
+            event.preventDefault.called.should.be.true;
+            event.preventDefault.reset();
+        });
+
+        it('should call changeVideo on the youtube player', function() {
+            let event = { preventDefault: sinon.stub() };
+            this.sut.submit(event);
+            this.sut.youtubePlayer.changeVideo.called.should.be.true;
+        });
     });
 });
