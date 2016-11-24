13454dc3764c06daf939d27ec5c90f4211e909ed
dnlsndbrg <dnlsndbrg@gmail.com>
2016-11-24 14:30:36 +0100

    :white_check_mark: implements getStringFromInput

--- src/client/InputForm.js
+++ src/client/InputForm.js
@@ -1,12 +1,18 @@
 class InputForm {
     constructor(input, button, youtubePlayer) {
         this.youtubePlayer = youtubePlayer;
+        this.input = input;
+        this.button = button;
         button.addEventListener('click', this.submit.bind(this));
     }
 
     submit(e) {
         e.preventDefault();
         this.youtubePlayer.changeVideo();
+    }
+
+    getStringFromInput() {
+        return this.input.value;
     }
 }
 
