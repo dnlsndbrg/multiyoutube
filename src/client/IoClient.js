ec81ac0da5f9d073b67f907b244f3661d4aa316b
Daniel Sundberg <dnlsndbrg@gmail.com>
2016-12-05 13:25:45 +0100

    removes untested code

--- src/client/IoClient.js
+++ src/client/IoClient.js
@@ -4,14 +4,6 @@ class IoClient {
     }
     connect(io) {
         this.socket = io(this.url);
-
-        this.socket.on('connect', function() {
-            console.log('connected');
-        });
-
-        this.socket.on('emit', function(data) {
-            console.log('io', data);
-        });
     }
 }
 
